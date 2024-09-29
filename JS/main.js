a736d65d29c7434b235fd8bc138f24f5cbc69bc1
Patrick Anndwin Clacio <patrick.clacio@acnnewswire.com>
2024-09-29 19:25:46 +0800

    fix the responsiveness, still wip

--- JS/main.js
+++ JS/main.js
@@ -1,43 +1,80 @@
-// Calling the header
+// FETCHING HEADER AND FOOTER
 document.addEventListener('DOMContentLoaded', function() {
+    // header
     fetch('header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header').innerHTML = data;
+
+            // To make the header sticky and fixed
+            window.addEventListener("scroll", function() {
+                const selectHeader = document.querySelector("#header.fixed-top");
+                const container =  document.querySelector("#header + div");
+
+                console.log(selectHeader);
+
+                if (selectHeader && container) {
+                    if (window.scrollY > 100) {
+                        selectHeader.classList.add("sticked");
+                        console.log(selectHeader);
+                        // container.style.marginTop = '240px'; // adjust container margin-top so that it won't be blocked on the first scroll
+                    } else {
+                        selectHeader.classList.remove("sticked");
+                        // container.style.marginTop = "0"; // reset margin-top
+                    }
+                }
+            });
+
+            // Call updateTime() after the new content is loaded
+            updateTime();
+            
+            // Optionally, set up the interval for continuous updates
+            setInterval(updateTime, 1000);
         })
         .catch(error => console.error('Error loading header:', error));
-});
 
-// To make the header sticky and fixed
-window.addEventListener("scroll", function() {
-    const header = document.querySelector(".header-container");
-    const stickyPoint = header.offsetTop;
-
-    if (window.pageYOffset > stickyPoint) {
-        header.classList.add("header-fixed");
-    } else {
-        header.classList.remove("header-fixed");
-    }
-});
-
-// Adjust header when scrolling
-window.addEventListener("scroll", function() {
-    const header = document.querySelector(".header-container");
-    const scrollPosition = window.scrollY;
-
-    if (scrollPosition > 50) {
-        header.classList.add("header-scroll");
-    } else {
-        header.classList.remove("header-scroll");
-    }
-});
-
-// Calling the footer
-document.addEventListener('DOMContentLoaded', function() {
+    // footer
     fetch('footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer').innerHTML = data;
         })
         .catch(error => console.error('Error loading footer:', error));
+    
+    AOS.init();
 });
+
+// TIMEZONES
+function updateTime() {
+    const localDate = getLocalDate();
+    document.getElementById('current-date').textContent = localDate;
+
+    const mumbaiTime = getTimePerTimezone('Asia/Kolkata');
+    document.getElementById('mumbai').textContent = mumbaiTime;
+
+    const bangkokTime = getTimePerTimezone('Asia/Bangkok');
+    document.getElementById('bangkok').textContent = bangkokTime;
+
+    const singaporeTime = getTimePerTimezone('Asia/Singapore');
+    document.getElementById('singapore').textContent = singaporeTime;
+
+    const hongkongTime = getTimePerTimezone('Asia/Hong_Kong');
+    document.getElementById('hongkong').textContent = hongkongTime;
+
+    const tokyoTime = getTimePerTimezone('Asia/Tokyo')
+    document.getElementById('tokyo').textContent = tokyoTime;
+
+    const sydneyTime = getTimePerTimezone('Australia/Sydney')
+    document.getElementById('sydney').textContent = sydneyTime;
+}
+
+function getLocalDate(){
+    const currentDate = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
+    return currentDate.toLocaleDateString(undefined, options);
+}
+
+function getTimePerTimezone(timeZone) {
+    const options = { hour: '2-digit', minute: '2-digit', hour12: false};
+    return new Date().toLocaleTimeString('en-US', { timeZone: timeZone, ...options });
+}
