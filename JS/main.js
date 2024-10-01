a4e8513dc6f15eeddb1c25f90f347ad478b0bbae
Patrick Anndwin Clacio <patrick.clacio@acnnewswire.com>
2024-10-01 20:21:26 +0800

    fix responsiveness and start with the api integration for live data

--- JS/main.js
+++ JS/main.js
@@ -9,14 +9,11 @@ document.addEventListener('DOMContentLoaded', function() {
             // To make the header sticky and fixed
             window.addEventListener("scroll", function() {
                 const selectHeader = document.querySelector("#header.fixed-top");
-                const container =  document.querySelector("#header + div");
-
-                console.log(selectHeader);
+                const container =  document.querySelector("#header + div") || document.querySelector("#header + section");
 
                 if (selectHeader && container) {
                     if (window.scrollY > 100) {
                         selectHeader.classList.add("sticked");
-                        console.log(selectHeader);
                         // container.style.marginTop = '240px'; // adjust container margin-top so that it won't be blocked on the first scroll
                     } else {
                         selectHeader.classList.remove("sticked");
@@ -42,6 +39,7 @@ document.addEventListener('DOMContentLoaded', function() {
         .catch(error => console.error('Error loading footer:', error));
     
     AOS.init();
+    fetchTopReleases();
 });
 
 // TIMEZONES
@@ -77,4 +75,39 @@ function getLocalDate(){
 function getTimePerTimezone(timeZone) {
     const options = { hour: '2-digit', minute: '2-digit', hour12: false};
     return new Date().toLocaleTimeString('en-US', { timeZone: timeZone, ...options });
+}
+
+// CAROUSEL
+function fetchTopReleases() {
+    const apiEndpoint = 'https://www.acnnewswire.com/acnnewswireapi/api/v1/News/EnglishNews'; 
+
+    fetch(apiEndpoint)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Response was NOT OK');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const topPressReleases = data.slice(2,5); // Get 3 press releases
+            console.log(topPressReleases);
+
+            document.getElementById('press-release-image-1').src = topPressReleases[0]?.photo[0]?.bigImage ?? '../Assets/home-page/temp-main-image.png';
+            document.getElementById('press-release-image-1').alt = topPressReleases[0]?.headline;
+            document.getElementById('press-release-header-1').textContent = topPressReleases[0]?.headline;
+            document.getElementById('press-release-summary-1').textContent = topPressReleases[0]?.description.split(/(?<=[.!?])\s/)[0];
+
+            document.getElementById('press-release-image-2').src = topPressReleases[1]?.photo[0]?.bigImage ?? '../Assets/home-page/temp-main-image.png';
+            document.getElementById('press-release-image-2').alt = topPressReleases[1]?.headline;
+            document.getElementById('press-release-header-2').textContent = topPressReleases[1]?.headline;
+            document.getElementById('press-release-summary-2').textContent = topPressReleases[1]?.description.split(/(?<=[.!?])\s/)[0];
+
+            document.getElementById('press-release-image-3').src = topPressReleases[2]?.photo[0]?.bigImage ?? '../Assets/home-page/temp-main-image.png';
+            document.getElementById('press-release-image-3').alt = topPressReleases[2]?.headline;
+            document.getElementById('press-release-header-3').textContent = topPressReleases[2]?.headline;
+            document.getElementById('press-release-summary-3').textContent = topPressReleases[2]?.description.split(/(?<=[.!?])\s/)[0];
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
